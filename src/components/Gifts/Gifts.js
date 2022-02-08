8d5a0989ffca5dafaa7c64133fe6e40147e07e50
Nicolas Gonzalez <ngonzalez@dminc.com>
2022-02-07 22:07:13 -0300

    Price updated

--- src/components/Gifts/Gifts.js
+++ src/components/Gifts/Gifts.js
@@ -5,7 +5,7 @@ export const Gifts = () => {
     <section id="regalos" className="px-4 mb-5">
       <h1 className="text-center">Regalos</h1>
       <div className="mt-5 row justify-content-center">
-        <h5 className='text-center col-12 col-md-8 col-lg-6'>Para nosotros tu presencia es lo más importante, pero si querés hacernos un regalo tendremos un buzón en el salón o la cuenta de C.B.U.</h5>
+        <h5 className='text-center col-12 col-md-8 col-lg-6'>Para nosotros tu presencia es lo más importante, pero si querés hacernos un regalo tendremos un buzón en el salón o la cuenta de CBU</h5>
       </div>
     </section>
   )
