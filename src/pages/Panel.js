57ef490f05572eef89c78955cafc9bc8fdf92c2e
Nicolas Gonzalez <ngonzalez@dminc.com>
2022-02-19 18:45:33 -0300

    tab pane added

--- src/pages/Panel.js
+++ src/pages/Panel.js
@@ -1,14 +1,32 @@
 import React from 'react'
-import { Link } from 'react-router-dom' 
+import { AllGuest } from '../components/AllGuest/AllGuest'
+import { PayInvited } from '../components/PayInvited/PayInvited'
 
 export const Panel = () => {
   return (
     <div className="p-4">
-      <h1 className="text-center mb-5">Panel Admin</h1>
-      <Link to="/admin/invite" type="button" className="btn btn-secondary w-sm-50 w-100 my-3">Invitar</Link>
-      <Link to="/admin/desinvite" type="button" className="btn btn-secondary w-sm-50 w-100 my-3">Desinvitar</Link>
-      <Link to="/admin/payment" type="button" className="btn btn-secondary w-sm-50 w-100 my-3">Pagar</Link>
-      <Link to="/admin/verify" type="button" className="btn btn-secondary w-sm-50 w-100 my-3">Verificar</Link>
+      <ul class="nav nav-tabs" id="myTab" role="tablist">
+        <li class="nav-item" role="presentation">
+          <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true">Todos los invitados</button>
+        </li>
+        <li class="nav-item" role="presentation">
+          <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false">Familia</button>
+        </li>
+        <li class="nav-item" role="presentation">
+          <button class="nav-link" id="contact-tab" data-bs-toggle="tab" data-bs-target="#contact" type="button" role="tab" aria-controls="contact" aria-selected="false">Pagados</button>
+        </li>
+      </ul>
+      <div class="tab-content" id="myTabContent">
+        <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+          <AllGuest />
+        </div>
+        <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+          <PayInvited />
+        </div>
+        <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">
+          Otra Tab
+        </div>
+      </div>
     </div>
   )
 }
